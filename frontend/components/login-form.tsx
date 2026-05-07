e17f0761b415184f31c82d0f5e683cf7f6daf64d
ThiDaZ <wthidas95@gmail.com>
2026-05-07 14:49:40 +0530

    feat:implement proxy to check session and setup tanstack query

--- frontend/components/login-form.tsx
+++ frontend/components/login-form.tsx
@@ -1,14 +1,31 @@
 import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import {
-	Field,
-	FieldGroup,
-	FieldLabel,
-} from "@/components/ui/field";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
+import { useState } from "react";
 
 export function LoginForm({ className, ...props }: React.ComponentProps<"div">) {
+
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
+
+
+	
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		console.log("Form submitted");
+
+		if (!email || !password) {
+			console.error("Email and password are required");
+			return;
+		}
+
+		
+		
+		console.log({ email, password });
+	};
+
 	return (
 		<div className={cn("flex flex-col gap-6", className)} {...props}>
 			<Card>
@@ -17,17 +34,30 @@ export function LoginForm({ className, ...props }: React.ComponentProps<"div">) 
 					<CardDescription>Login with your email and password</CardDescription>
 				</CardHeader>
 				<CardContent>
-					<form>
+					<form onSubmit={handleSubmit}>
 						<FieldGroup>
 							<Field>
 								<FieldLabel htmlFor="email">Email</FieldLabel>
-								<Input id="email" type="email" placeholder="m@example.com" required />
+								<Input
+									id="email"
+									type="email"
+									value={email}
+									onChange={(e) => setEmail(e.target.value)}
+									placeholder="m@example.com"
+									required
+								/>
 							</Field>
 							<Field>
 								<div className="flex items-center">
 									<FieldLabel htmlFor="password">Password</FieldLabel>
 								</div>
-								<Input id="password" type="password" required />
+								<Input
+									id="password"
+									type="password"
+									value={password}
+									onChange={(e) => setPassword(e.target.value)}
+									required
+								/>
 							</Field>
 							<Field>
 								<Button type="submit">Login</Button>
