6cab080c0622fa1e511a9f630ca63928e379db1c
ThiDaZ <wthidas95@gmail.com>
2026-05-08 08:40:00 +0530

    feat: add CRUD operations for leads;

--- frontend/lib/api/leads/leads.ts
+++ frontend/lib/api/leads/leads.ts
@@ -22,3 +22,68 @@ export const createLead = async (newLead: Lead) => {
 	}
 	return response.json();
 };
+
+export const getLeads = async () => {
+	const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+	if (!apiUrl) {
+		throw new Error("API URL is not defined in environment variables");
+	}
+
+	const response = await fetch(`${apiUrl}/leads`, {
+		method: "GET",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		credentials: "include",
+	});
+
+	if (!response.ok) {
+		const errorResponse = await response.json();
+		const errorMessage = errorResponse.error ?? "Failed to fetch leads";
+		throw new Error(errorMessage);
+	}
+	return response.json();
+};
+
+export const updateStatus = async (leadId: string, newStatus: string) => {
+	const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+	if (!apiUrl) {
+		throw new Error("API URL is not defined in environment variables");
+	}
+	const response = await fetch(`${apiUrl}/leads/${leadId}/status`, {
+		method: "PUT",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ status: newStatus }),
+		credentials: "include",
+	});
+
+	if (!response.ok) {
+		const errorResponse = await response.json();
+		const errorMessage = errorResponse.error ?? "Failed to update lead status";
+		throw new Error(errorMessage);
+	}
+	return response.json();
+};
+
+export const deleteLead = async (leadId: string) => {
+	const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+	if (!apiUrl) {
+		throw new Error("API URL is not defined in environment variables");
+	}
+	const response = await fetch(`${apiUrl}/leads/${leadId}`, {
+		method: "DELETE",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		credentials: "include",
+	});
+
+	if (!response.ok) {
+		const errorResponse = await response.json();
+		const errorMessage = errorResponse.error ?? "Failed to delete lead";
+		throw new Error(errorMessage);
+	}
+	return response.json();
+};
