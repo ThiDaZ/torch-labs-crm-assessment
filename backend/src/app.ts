4a5c7eedba9fd8b4d37d2213b7f523f540e9c33f
ThiDaZ <wthidas95@gmail.com>
2026-05-11 18:15:16 +0530

    fix: update CORS origin to use environment variable

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -6,12 +6,13 @@ import leadsRouter from "./routes/leads.routes.ts";
 import noteRouter from "./routes/note.routes.ts";
 import dashboardRouter from "./routes/dashboard.routes.ts";
 import userRouter from "./routes/user.routes.ts";
+import 'dotenv/config'
 
 const app: express.Application = express();
 
 app.use(
 	cors({
-		origin: process.env.FRONTEND_URL || "http://localhost:3000",
+		origin: process.env.FRONTEND_URL,
     // CRITICAL: Must be true to allow Next.js proxy to read the JWT cookie
 		credentials: true,
 		methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
